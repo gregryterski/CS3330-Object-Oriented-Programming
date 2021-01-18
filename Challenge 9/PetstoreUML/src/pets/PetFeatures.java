9220d972c221be9275fa3ad0f04cac7703f10968
Greg Ryterski <55214916+gregryterski@users.noreply.github.com>
2021-01-18 14:00:35 -0600

    Add files via upload

---
+++ Challenge 9/PetstoreUML/src/pets/PetFeatures.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pets;
+
+/**
+ *
+ * @author Professor Wergeles <Professor Wergeles at cs3330@missouri.edu>
+ */
+public interface PetFeatures {
+    boolean hasTail(); 
+    boolean canFly();
+    boolean hasFeather();
+}    
